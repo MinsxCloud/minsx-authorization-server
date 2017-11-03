3775a3a3c2f85195dd915f8bc07b81c193c788d6
JOKERPC\web <goodsave@qq.com>
2017-11-03 20:52:10 +0800

    no message

--- minsx-authorization-starter/src/test/java/org/minsx/authorization/starter/ApplicationStarterTest.java
+++ minsx-authorization-starter/src/test/java/org/minsx/authorization/starter/ApplicationStarterTest.java
@@ -1,38 +1,10 @@
 package org.minsx.authorization.starter;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+public class ApplicationStarterTest {
 
-/**
- * Unit test for simple App.
- */
-public class ApplicationStarterTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public ApplicationStarterTest( String testName )
-    {
-        super( testName );
+    public static void main(String[] args){
+
+        System.out.println("china");
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( ApplicationStarterTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
 }
