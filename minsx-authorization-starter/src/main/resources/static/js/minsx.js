8d7610389928beb58e83441e36ad0f8d0dfc651a
JOKERPC\web <goodsave@qq.com>
2017-12-18 00:28:34 +0800

    登录补充

--- minsx-authorization-starter/src/main/resources/static/js/minsx.js
+++ minsx-authorization-starter/src/main/resources/static/js/minsx.js
@@ -39,13 +39,42 @@ Minsx = {
             });
         }
     },
-    Http:{
-        get:function (url,async) {
-            xmlhttp.open("GET",url,async);
+    Http: {
+        get: function (url, async) {
+            xmlhttp.open("GET", url, async);
         },
-        post:function (url,async){
-            xmlhttp.open("POST",url,async);
-            xmlhttp.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+        post: function (url, async) {
+            xmlhttp.open("POST", url, async);
+            xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        }
+    },
+    Cookie: {
+        set: function () {
+            var args = arguments;
+            if (args.length === 2) {
+                document.cookie = args[0] + "=" + escape(args[1]);
+            } else if (args.length === 3) {
+                document.cookie = args[0] + "=" + escape(args[1]) + ";path=" + args[2];
+            } else if (args.length === 4) {
+                document.cookie = args[0] + "=" + escape(args[1]) + ";path=" + args[2] + ";domain=" + args[3];
+            }
+        },
+        get: function (key) {
+            var arr, reg = new RegExp("(^| )" + key + "=([^;]*)(;|$)");
+            if (arr = document.cookie.match(reg)) {
+                return unescape(arr[2]);
+            } else {
+                return null;
+            }
+        },
+        delete: function (key) {
+            var exp = new Date();
+            exp.setTime(exp.getTime() - 1);
+            var cval = getCookie(key);
+            if (cval !== null) {
+                document.cookie = name + "=" + cval + ";expires=" + exp.toGMTString();
+            }
         }
     }
 }
+
